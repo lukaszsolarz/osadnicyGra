e9b4a010ae8a123ba00d64d63fcf15b27d9c3ba4
Lukas <lukaszsolarz@icloud.com>
2019-11-05 13:47:59 +0100

    dodaje poprawę do napisu DREWNO

--- gra.php
+++ gra.php
@@ -1,13 +1,13 @@
 <?php
 
 	session_start();
-	
+
 	if (!isset($_SESSION['zalogowany']))
 	{
 		header('Location: index.php');
 		exit();
 	}
-	
+
 ?>
 <!DOCTYPE HTML>
 <html lang="pl">
@@ -18,17 +18,17 @@
 </head>
 
 <body>
-	
+
 <?php
 
 	echo "<p>Witaj serdecznie  ".$_SESSION['user'].'! [ <a href="logout.php">Wyloguj się!</a> ]</p>';
-	echo "<p><b>Drewno</b>: ".$_SESSION['drewno'];
+	echo "<p><b>Drewmno poprawka</b>: ".$_SESSION['drewno'];
 	echo " | <b>Kamień</b>: ".$_SESSION['kamien'];
 	echo " | <b>Zboże</b>: ".$_SESSION['zboze']."</p>";
-	
+
 	echo "<p><b>E-mail</b>: ".$_SESSION['email'];
 	echo "<br /><b>Dni premium</b>: ".$_SESSION['dnipremium']."</p>";
-	
+
 ?>
 
 </body>
